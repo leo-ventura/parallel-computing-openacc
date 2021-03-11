4f7e7db111cdd2ddca6e19acd5940d7370bc02d4
Miguel Angelo <masbicudo@gmail.com>
2021-03-11 16:06:39 -0300

    calcpi - usando trapézios - corrigido + cli para dizer número de divisões

--- src/pi_acc.c
+++ src/pi_acc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 #include <time.h>
 
@@ -11,32 +12,36 @@ struct timespec timer_start() {
 }
 
 // call this function to end a timer, returning nanoseconds elapsed as a long
-long timer_end(struct timespec start_time){
+long long timer_end(struct timespec start_time){
     struct timespec end_time;
     clock_gettime(CLOCK_MONOTONIC, &end_time);
-    long diffInNanos = (end_time.tv_sec - start_time.tv_sec) * (long)1e9 + (end_time.tv_nsec - start_time.tv_nsec);
+    long long diffInNanos = (end_time.tv_sec - start_time.tv_sec) * (long long)1e9 + (end_time.tv_nsec - start_time.tv_nsec);
     return diffInNanos;
 }
 
 int main(int argc, char *argv[]) { /* acc_calcpi.c  */
     double pi = 0.0f;
-    long N=1000000000;
+    long long N = 1000000000;
+
+    printf("%d\n", argc);
+    if (argc == 2)
+        N = atoll(argv[1]);
 
     struct timespec vartime = timer_start();
 
     #pragma acc parallel loop reduction(+: pi)
-    for (long i = 0; i < N + 1; i++) {
+    for (long long i = 0; i < N + 1; i++) {
         double t = (double)i/N;
         pi += 1.0/(1.0 + t*t);
    	}
-    pi -= 1.0/(1.0 + 0.0*0.0) + 1.0/(1.0 + 1.0*1.0);
-    pi *= 0.5/(N + 1);
-    pi *= 4;
+    pi -= 0.5*(1.0/(1.0 + 0.0*0.0) + 1.0/(1.0 + 1.0*1.0));
+    pi *= 4.0/(N + 1);
 
-    long time_elapsed_nanos = timer_end(vartime);
+    long long time_elapsed_nanos = timer_end(vartime);
+    printf("%lld\n", time_elapsed_nanos);
     double elapsed = time_elapsed_nanos*0.000000001;
     printf("%lf\n", elapsed);
 
-    printf("O valor de pi é: %f\n",pi/N);
+    printf("O valor de pi é: %.12f\n",pi);
     return(0);
 }
